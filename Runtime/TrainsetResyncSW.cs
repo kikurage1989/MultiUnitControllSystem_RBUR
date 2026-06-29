8b520093cc32f143e384342c1106a43804ac3dc6
kikurage1989 <kikurage1989.ki@gmail.com>
2026-06-29 21:53:16 +0900

    TrainsetResyncSW

---
+++ Runtime/TrainsetResyncSW.cs
@@ -0,0 +1,16 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using frou01.RigidBodyTrain;
+using frou01.GrabController;
+using ragecraft.UtilsScript;
+
+namespace ragecraft.MultiUnitControllSystem_RBUR
+{
+    public class TrainsetResyncSW : UdonSharpBehaviour
+    {
+        
+    }
+}
